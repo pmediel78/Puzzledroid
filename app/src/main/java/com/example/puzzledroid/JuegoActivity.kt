fe29066ee5b62889b0fc867a7167c68a3044b241
Pedro <pmediel.bcnbit@gmail.com>
2021-10-23 17:12:52 +0200

    Refactor

--- app/src/main/java/com/example/puzzledroid/JuegoActivity.kt
+++ app/src/main/java/com/example/puzzledroid/JuegoActivity.kt
@@ -1,13 +1,10 @@
 package com.example.puzzledroid
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Half.toFloat
 import android.view.View
-import android.widget.Button
+import com.google.android.material.card.MaterialCardView
 import kotlinx.android.synthetic.main.activity_juego.*
-import kotlinx.android.synthetic.main.activity_main2.*
 import kotlinx.android.synthetic.main.activity_main2.draggableCard1
 import kotlinx.android.synthetic.main.activity_main2.draggableCard2
 import kotlinx.android.synthetic.main.activity_main2.draggableCard3
@@ -38,38 +35,18 @@ class JuegoActivity : AppCompatActivity() {
             }
 
             override fun onViewReleased(view: View, v: Float, v1: Float) {
-                when (view.id) {
-                    R.id.draggableCard1 -> draggableCard1.isDragged = false
-                    R.id.draggableCard2 -> draggableCard2.isDragged = false
-                    R.id.draggableCard3 -> draggableCard3.isDragged = false
-                    R.id.draggableCard4 -> draggableCard4.isDragged = false
-                }
+                var x: Float = 0f
+                var y: Float = 0f
+                var dist = 100
 
-                var x : Float = 0f
-                var y : Float = 0f
 
-                if (view.getTag() == "card1") {
-                    println(1)
-                    x = fondo1.x
-                    y = fondo1.y
-                }
-                if (view.getTag() == "card2") {
-                    println(2)
-                    x = fondo2.x
-                    y = fondo2.y
-                }
-                if (view.getTag() == "card3") {
-                    println(3)
-                    x = fondo3.x
-                    y = fondo3.y
-                }
-                if (view.getTag() == "card4") {
-                    println(4)
-                    x = fondo4.x
-                    y = fondo4.y
-                }
+                var name = resources.getResourceEntryName(view.id)
+                var position = parentCoordinatorLayout.findViewWithTag<MaterialCardView>(name)
 
-                if(x - 100 < view.x && x + 100 > view.x && y - 100 < view.y && y + 100 > view.y){
+                x = position.x
+                y = position.y
+
+                if (x - dist < view.x && x + dist > view.x && y - dist < view.y && y + dist > view.y) {
                     view.x = x
                     view.y = y
                 }
@@ -77,7 +54,8 @@ class JuegoActivity : AppCompatActivity() {
                 if (draggableCard1.x == fondo1.x &&
                     draggableCard2.x == fondo2.x &&
                     draggableCard3.x == fondo3.x &&
-                    draggableCard4.x == fondo4.x ) {
+                    draggableCard4.x == fondo4.x
+                ) {
                     println("GANA")
                 }
             }
