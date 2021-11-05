99e2504453983c6b15dd2883885090081861e075
marcvil <marcvilaespunya@gmail.com>
2021-11-05 18:12:38 +0100

    Database

---
+++ app/src/main/java/com/example/puzzledroid/UserContract.kt
@@ -0,0 +1,43 @@
+package com.example.puzzledroid
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.provider.BaseColumns
+
+object UserContract {
+    // Table contents are grouped together in an anonymous object.
+    object UserEntry : BaseColumns {
+        const val TABLE_NAME = "UserTable"
+        const val COLUMN_NAME_USERNAME = "Name"
+        const val COLUMN_NAME_SCORE = "Score"
+    }
+
+    private const val SQL_CREATE_ENTRIES =
+        "CREATE TABLE ${UserEntry.TABLE_NAME} (" +
+                "${BaseColumns._ID} INTEGER PRIMARY KEY," +
+                "${UserEntry.COLUMN_NAME_USERNAME} TEXT," +
+                "${UserEntry.COLUMN_NAME_SCORE} INT)"
+
+    private const val SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS ${UserEntry.TABLE_NAME}"
+
+    class UserDbHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+        override fun onCreate(db: SQLiteDatabase) {
+            db.execSQL(SQL_CREATE_ENTRIES)
+        }
+        override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+            // This database is only a cache for online data, so its upgrade policy is
+            // to simply to discard the data and start over
+            db.execSQL(SQL_DELETE_ENTRIES)
+            onCreate(db)
+        }
+        override fun onDowngrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+            onUpgrade(db, oldVersion, newVersion)
+        }
+        companion object {
+            // If you change the database schema, you must increment the database version.
+            const val DATABASE_VERSION = 1
+            const val DATABASE_NAME = "UserTable.db"
+        }
+    }
+}
---
+++ app/src/main/java/com/example/puzzledroid/WinActivity.kt
@@ -0,0 +1,73 @@
+package com.example.puzzledroid
+
+import android.content.ContentValues
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.BaseColumns
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputLayout
+import kotlinx.android.synthetic.main.activity_win.*
+
+class WinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_win)
+
+        SetButtonOnClickListeners()
+    }
+    fun SetButtonOnClickListeners() {
+        JuegoButtonClick()
+    }
+
+    private fun JuegoButtonClick() {
+        val juegoBtn: Button = findViewById(R.id.Juegobtn)
+        juegoBtn.setOnClickListener {
+            val Contador_1= intent.getStringExtra("Counter")?.toInt()
+            var Counter = Contador_1
+
+            val name = NameToSave.editText?.text.toString()
+
+
+
+            val dbHelper = UserContract.UserDbHelper(applicationContext)
+
+            val db = dbHelper.writableDatabase
+
+            // Create a new map of values, where column names are the keys
+            val values = ContentValues().apply {
+                put(UserContract.UserEntry.COLUMN_NAME_USERNAME, name)
+                put(UserContract.UserEntry.COLUMN_NAME_SCORE, Counter)
+            }
+
+            // Insert the new row, returning the primary key value of the new row
+            val newRowId = db?.insert(UserContract.UserEntry.TABLE_NAME, null, values)
+
+
+            val dbread = dbHelper.readableDatabase
+
+            // Define a projection that specifies which columns from the database
+            // you will actually use after this query.
+            val projection = arrayOf(BaseColumns._ID, UserContract.UserEntry.COLUMN_NAME_USERNAME, UserContract.UserEntry.COLUMN_NAME_SCORE)
+
+
+            // How you want the results sorted in the resulting Cursor
+            val sortOrder = "${UserContract.UserEntry.COLUMN_NAME_SCORE} DESC"
+
+            val valuesDatabase = db.query(
+                UserContract.UserEntry.TABLE_NAME,   // The table to query
+                projection,             // The array of columns to return (pass null to get all)
+                null,              // The columns for the WHERE clause
+                null,          // The values for the WHERE clause
+                null,                   // don't group the rows
+                null,                   // don't filter by row groups
+                sortOrder               // The sort order
+            )
+            val intent = Intent(this, RankingActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+
+}
